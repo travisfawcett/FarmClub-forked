188e6a91fe5c80e2ab3504c8d7d4307d890b500a
IsabelGAP <igonzalez@wearegap.com>
2019-09-05 08:30:40 -0600

    Test commit

--- app/Http/Resources/Stores.php
+++ app/Http/Resources/Stores.php
@@ -9,10 +9,9 @@ class Stores extends JsonResource
     /**
      * Transform the resource into an array.
      *
-     * @param  \Illuminate\Http\Request  $request
      * @return array
      */
-    public function toArray($request)
+    public function toArray()
     {
         return [
             'id' => $this->id,
